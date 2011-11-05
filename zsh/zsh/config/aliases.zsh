e5e12e643a53ac4e7bd654c79c7f2a1392a38634
Daniel Neuhäuser <dasdasich@gmail.com>
2011-11-05 17:26:58 +0100

    Add aliases for cabal-dev
    cabal -> cabal-global
    cabal-dev -> cabal
    cabal-dev ghci -> ghci
    cabal-dev ghc-pkg -> ghc-pkg

--- zsh/zsh/config/aliases.zsh
+++ zsh/zsh/config/aliases.zsh
@@ -7,3 +7,42 @@ if [ -e ~/bin/hub ]
 then
 	alias git=hub
 fi
+
+# cabal             -> cabal-global
+# cabal-dev         -> cabal
+# cabal-dev ghci    -> ghci
+# cabal-dev ghc-pkg -> ghc-pkg
+#
+# This keeps everything in the current sandbox, which is the sane way to go
+# for something that's supposed to be a build tool and not a package manager
+_ghci() {
+	# launches cabal-dev ghci if file matching *.cabal is found in the current
+	# directory or ghci if not
+	FILTER=$(find . -maxdepth 1 -type f \( -name "*.cabal" \) )
+	if [ -z ${FILTER} ]
+	then
+		ghci
+	else
+		cabal-dev ghci
+	fi
+}
+
+_ghc_pkg() {
+	# launches cabal-dev ghc-pkg if a "cabal-dev" directory is found or ghc-pkg
+	# if not
+	if [ -e "cabal-dev" ]
+	then
+		cabal-dev ghc-pkg
+	else
+		ghc-pkg
+	fi
+}
+
+command -v cabal-dev >> /dev/null
+if [ $? -eq 0 ]
+then
+	alias cabal-global=`which cabal`
+	alias cabal=cabal-dev
+	alias ghci=_ghci
+	alias ghc-pkg=_ghc_pkg
+fi
